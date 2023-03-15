aceade9d76b2bc3d5ea5140489d3c6c4d1885df7
@Wang-Ruping11 <1956815917@qq.com>
2023-03-15 13:51:48 +0800

    HW-Week 2 Commit

--- src/main/java/com/wrp/week2/demo/HelloWorldServlet.java
+++ src/main/java/com/wrp/week2/demo/HelloWorldServlet.java
@@ -1,6 +1,17 @@
 package com.wrp.week2.demo;
-
-public class HelloWorldServlet extends HttpServlet {
-    public void  doGet(HttpServletquest request,HttpServletR)
-
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+public class HelloWorldServlet extends HttpServlet{
+    public void doGet(HttpServletRequest request,HttpServletResponse response)
+            throws IOException {
+        //when client request method is Get - here - inside doGet()
+        PrintWriter writer =response.getWriter();
+        writer.println("Hello Client!!!");
+    }
+    public void doPost(HttpServletRequest request,HttpServletResponse response){
+        //when client request method is Post - here - inside doPost()
+    }
 }
